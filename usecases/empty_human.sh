a5c220bc35c09070b0781058a90017ee927da9eb
espetro <quino.terrasa+dev@gmail.com>
2019-06-01 11:41:51 +0200

    Updated to last Sphinx version (works for Anafi4k). Extended empty_human usecase to work for both bebop2 and anafi.

--- usecases/empty_human.sh
+++ usecases/empty_human.sh
@@ -2,5 +2,18 @@
 
 # This script creates an empty world with a Bebop 2 and a human doing rounds
 
-sphinx $SPHINX_ROOT/drones/local_bebop2.drone \
-       $SPHINX_ROOT/actors/pedestrian.actor::machine=bebop2::path=$SPHINX_ROOT/paths/square_walk.path
+printf "Which drone do u want to use?\n1 - Bebop2\n2 - Anafi\n"
+
+read drone_type
+
+if [ $drone_type -eq 1 ]
+then
+    drone_name="bebop2"
+    drone="local_bebop2.drone"
+else
+    drone_name="anafi4k"
+    drone="local_anafi4k.drone"
+fi
+
+sphinx $SPHINX_ROOT/drones/$drone \
+       $SPHINX_ROOT/actors/pedestrian.actor::machine=$drone_name::path=$SPHINX_ROOT/paths/square_walk.path
