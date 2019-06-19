9a7c13bbd21fa6130eb0ab98fac73094fa2c2745
espetro <quino.terrasa+dev@gmail.com>
2019-06-19 19:48:43 +0200

    Modified world camera poses. Added a name to pedestrian in usecases.

--- usecases/simple_cross_door.sh
+++ usecases/simple_cross_door.sh
@@ -3,4 +3,4 @@
 # --datalog
 sphinx $SPHINX_ROOT/worlds/simple_cross_door.world \
        $SPHINX_ROOT/drones/local_bebop2.drone \
-       $SPHINX_ROOT/actors/pedestrian.actor::machine=bebop2::path=$SPHINX_ROOT/paths/simple_cross_door.path
+       $SPHINX_ROOT/actors/pedestrian.actor::name=main_actor::machine=bebop2::path=$SPHINX_ROOT/paths/simple_cross_door.path
